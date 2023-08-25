d41c6dfe5588c5e3b818008ec71d3dea17edb38e
Noise290 <noisemonster29@gmail.com>
2023-08-25 20:45:41 +0200

    yo

--- app/src/main/java/com/example/trivia/Trivia.java
+++ app/src/main/java/com/example/trivia/Trivia.java
@@ -1,4 +1,13 @@
 package com.example.trivia;
 
-public class Trivia {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Trivia extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
 }
